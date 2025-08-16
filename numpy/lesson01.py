4637267697ab40480abcc508db7351b21849b4ef
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-16 14:29:38 +0300

    Plot the housing prices

---
+++ numpy/lesson01.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+my_array = np.array([[1, 2, 3, 4],
+                     [5, 6, 7, 8],
+                     [9, 10, 11, 12],
+                     [13, 14, 15, 16]])
+
+print(my_array[1:3, 1:3])
